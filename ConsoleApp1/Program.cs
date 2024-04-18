3bf6efdfd23a7ad980e19c29397f84958d991cfb
Volcano-MP <149071205+Project-Z1K@users.noreply.github.com>
2024-04-18 13:23:12 -0700

    remake the game

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,48 +1,47 @@
 using System;
-using System.Diagnostics;
 
-class BatallaNaval
+class Battleship
 {
-    const int TAMANIO_TABLERO = 10;
-    static char[,] tablero = new char[TAMANIO_TABLERO, TAMANIO_TABLERO];
-    static int barcosRestantes = 20;
-    static int disparosRestantes = 50;
+    const int BOARD_SIZE = 10;
+    static char[,] board = new char[BOARD_SIZE, BOARD_SIZE];
+    static int shipsRemaining = 20;
+    static int shotsRemaining = 50;
 
-    static void InicializarTablero()
+    static void InitializeBoard()
     {
-        for (int i = 0; i < TAMANIO_TABLERO; ++i)
+        for (int i = 0; i < BOARD_SIZE; ++i)
         {
-            for (int j = 0; j < TAMANIO_TABLERO; ++j)
+            for (int j = 0; j < BOARD_SIZE; ++j)
             {
-                tablero[i, j] = '-';
+                board[i, j] = '-';
             }
         }
     }
 
-    static void ImprimirTablero()
+    static void PrintBoard()
     {
         Console.Write("  ");
-        for (int i = 0; i < TAMANIO_TABLERO; ++i)
+        for (int i = 0; i < BOARD_SIZE; ++i)
         {
-            Console.Write($"\x1b[33m{i}\x1b[0m ");
+            Console.Write($"{i} ");
         }
         Console.WriteLine();
-        for (int i = 0; i < TAMANIO_TABLERO; ++i)
+        for (int i = 0; i < BOARD_SIZE; ++i)
         {
-            Console.Write($"\x1b[33m{i}\x1b[0m ");
-            for (int j = 0; j < TAMANIO_TABLERO; ++j)
+            Console.Write($"{i} ");
+            for (int j = 0; j < BOARD_SIZE; ++j)
             {
-                if (tablero[i, j] == 'X')
+                if (board[i, j] == 'X')
                 {
-                    Console.Write("\x1b[36mX\x1b[0m ");
+                    Console.Write("X ");
                 }
-                else if (tablero[i, j] == 'H')
+                else if (board[i, j] == 'H')
                 {
-                    Console.Write("\x1b[31mH\x1b[0m ");
+                    Console.Write("H ");
                 }
-                else if (tablero[i, j] == 'O')
+                else if (board[i, j] == 'O')
                 {
-                    Console.Write("\x1b[34mO\x1b[0m ");
+                    Console.Write("O ");
                 }
                 else
                 {
@@ -54,160 +53,147 @@ class BatallaNaval
         Console.WriteLine();
     }
 
-    static void ColocarBarcosAleatorios()
+    static void PlaceRandomShips()
     {
         Random rnd = new Random();
-        int barcos = 0;
-        while (barcos < barcosRestantes)
+        int ships = 0;
+        while (ships < shipsRemaining)
         {
-            int fila = rnd.Next(0, TAMANIO_TABLERO);
-            int columna = rnd.Next(0, TAMANIO_TABLERO);
-            if (tablero[fila, columna] == '-')
+            int row = rnd.Next(0, BOARD_SIZE);
+            int column = rnd.Next(0, BOARD_SIZE);
+            if (board[row, column] == '-')
             {
-                tablero[fila, columna] = 'X';
-                barcos++;
+                board[row, column] = 'X';
+                ships++;
             }
         }
     }
 
-    static void JugarBatallaNaval()
+    static void PlayBattleship()
     {
-        InicializarTablero();
-        ColocarBarcosAleatorios();
+        InitializeBoard();
+        PlaceRandomShips();
 
-        while (disparosRestantes > 0)
+        while (shotsRemaining > 0)
         {
-            Console.WriteLine($"Disparos restantes: {disparosRestantes}");
-            ImprimirTablero();
-            Console.Write("Ingresa la fila para disparar: ");
-            int fila = int.Parse(Console.ReadLine());
-            Console.Write("Ingresa la columna para disparar: ");
-            int columna = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Shots remaining: {shotsRemaining}");
+            PrintBoard();
+            Console.Write("Enter the row to shoot: ");
+            int row = int.Parse(Console.ReadLine());
+            Console.Write("Enter the column to shoot: ");
+            int column = int.Parse(Console.ReadLine());
 
-            if (fila < 0 || fila >= TAMANIO_TABLERO || columna < 0 || columna >= TAMANIO_TABLERO)
+            if (row < 0 || row >= BOARD_SIZE || column < 0 || column >= BOARD_SIZE)
             {
-                Console.WriteLine("Disparo fuera de rango. Inténtalo de nuevo.");
+                Console.WriteLine("Shot out of range. Try again.");
                 continue;
             }
 
-            if (tablero[fila, columna] == 'X')
+            if (board[row, column] == 'X')
             {
-                tablero[fila, columna] = 'H';
-                Console.WriteLine("¡Has hundido un barco!");
-                barcosRestantes--;
+                board[row, column] = 'H';
+                Console.WriteLine("You've sunk a ship!");
+                shipsRemaining--;
             }
-            else if (tablero[fila, columna] == '-')
+            else if (board[row, column] == '-')
             {
-                tablero[fila, columna] = 'O';
-                Console.WriteLine("Agua... ¡Intenta con otro disparo!");
+                board[row, column] = 'O';
+                Console.WriteLine("Miss... Try another shot!");
             }
-            else if (tablero[fila, columna] == 'H' || tablero[fila, columna] == 'O')
+            else if (board[row, column] == 'H' || board[row, column] == 'O')
             {
-                Console.WriteLine("Ya has disparado en esta posición. Inténtalo de nuevo.");
+                Console.WriteLine("You've already shot at this position. Try again.");
                 continue;
             }
 
-            disparosRestantes--;
+            shotsRemaining--;
 
-            if (barcosRestantes == 0)
+            if (shipsRemaining == 0)
             {
-                ImprimirTablero();
-                Console.WriteLine("¡Felicidades! ¡Has hundido todos los barcos!");
+                PrintBoard();
+                Console.WriteLine("Congratulations! You've sunk all the ships!");
                 break;
             }
         }
 
-        if (disparosRestantes == 0 && barcosRestantes > 0)
+        if (shotsRemaining == 0 && shipsRemaining > 0)
         {
-            Console.WriteLine("¡Te has quedado sin disparos! ¡Has perdido!");
-            Console.WriteLine($"Barcos restantes: {barcosRestantes}");
+            Console.WriteLine("Out of shots! You've lost!");
+            Console.WriteLine($"Ships remaining: {shipsRemaining}");
         }
     }
 
-    static void MostrarCreditos()
+    static void ShowCredits()
     {
-        Console.WriteLine("***** CREDITOS *****");
-        Console.WriteLine("Desarrollado por:");
+        Console.WriteLine("***** CREDITS *****");
+        Console.WriteLine("Developed by:");
         Console.WriteLine("Andreu1k");
-        Console.WriteLine("andreu1k on yt");
+        Console.WriteLine("andreu1k on YouTube");
         Console.WriteLine("*********************");
     }
 
-    static void AbrirCanalYT()
+    static void OpenYouTubeChannel()
     {
-        ProcessStartInfo psi = new ProcessStartInfo
-        {
-            FileName = "cmd",
-            WindowStyle = ProcessWindowStyle.Hidden,
-            RedirectStandardInput = true,
-            UseShellExecute = false,
-            CreateNoWindow = true
-        };
-
-        Process proc = Process.Start(psi);
-        if (proc != null)
-        {
-            proc.StandardInput.WriteLine("start https://www.youtube.com/channel/UC1-tOvP6-SGgyxJwD9nZiPQ");
-        }
+        System.Diagnostics.Process.Start("https://www.youtube.com/channel/UC1-tOvP6-SGgyxJwD9nZiPQ");
     }
 
     static void Main()
     {
         while (true)
         {
-            Console.WriteLine("*** BATALLA NAVAL ***");
-            Console.WriteLine("1. Jugar");
-            Console.WriteLine("2. Dificultad");
-            Console.WriteLine("3. Créditos");
-            Console.WriteLine("4. Abrir Canal de YouTube");
-            Console.WriteLine("5. Salir");
-            Console.Write("Elige una opción: ");
-            int opcion = int.Parse(Console.ReadLine());
+            Console.WriteLine("*** BATTLESHIP ***");
+            Console.WriteLine("1. Play");
+            Console.WriteLine("2. Difficulty");
+            Console.WriteLine("3. Credits");
+            Console.WriteLine("4. Open YouTube Channel");
+            Console.WriteLine("5. Exit");
+            Console.Write("Choose an option: ");
+            int option = int.Parse(Console.ReadLine());
 
-            switch (opcion)
+            switch (option)
             {
                 case 1:
-                    barcosRestantes = 20;
-                    disparosRestantes = 50;
-                    JugarBatallaNaval();
+                    shipsRemaining = 20;
+                    shotsRemaining = 50;
+                    PlayBattleship();
                     break;
                 case 2:
-                    Console.WriteLine("Niveles de dificultad:");
-                    Console.WriteLine("1. Fácil");
-                    Console.WriteLine("2. Medio");
-                    Console.WriteLine("3. Difícil");
-                    Console.Write("Elige un nivel de dificultad: ");
-                    int nivel = int.Parse(Console.ReadLine());
-                    switch (nivel)
+                    Console.WriteLine("Difficulty levels:");
+                    Console.WriteLine("1. Easy");
+                    Console.WriteLine("2. Medium");
+                    Console.WriteLine("3. Hard");
+                    Console.Write("Choose a difficulty level: ");
+                    int level = int.Parse(Console.ReadLine());
+                    switch (level)
                     {
                         case 1:
-                            barcosRestantes = 15;
-                            disparosRestantes = 40;
+                            shipsRemaining = 15;
+                            shotsRemaining = 40;
                             break;
                         case 2:
-                            barcosRestantes = 20;
-                            disparosRestantes = 50;
+                            shipsRemaining = 20;
+                            shotsRemaining = 50;
                             break;
                         case 3:
-                            barcosRestantes = 25;
-                            disparosRestantes = 60;
+                            shipsRemaining = 25;
+                            shotsRemaining = 60;
                             break;
                         default:
-                            Console.WriteLine("Opción inválida. Seleccionando nivel medio por defecto.");
+                            Console.WriteLine("Invalid option. Selecting medium level by default.");
                             break;
                     }
                     break;
                 case 3:
-                    MostrarCreditos();
+                    ShowCredits();
                     break;
                 case 4:
-                    AbrirCanalYT();
+                    OpenYouTubeChannel();
                     break;
                 case 5:
                     Environment.Exit(0);
                     break;
                 default:
-                    Console.WriteLine("Opción inválida. Inténtalo de nuevo.");
+                    Console.WriteLine("Invalid option. Try again.");
                     break;
             }
         }
